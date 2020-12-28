a57b127abfff18dab335d8ff673b4187b547bd05
Chris P <chrisp1877@gmail.com>
2020-12-27 19:01:36 -0800

    updating install script, updating tmux conf

--- install.sh
+++ install.sh
@@ -33,7 +33,8 @@ install_fedora() {
     \ slurp sgtk-menu nwg-launchers vim-enhanced runc ripgrep fd-find pavucontrol pass
     \ mako kanshi imv vimiv mpv zathura dunst dnf-utils dnf-plugins-core bemenu azote
     \ yank dnf-plugins-core inotify-tools wayfire wayfire-devel glibmm24 glibmm24-devel
-    \ wlroots-devel wlroots wf-config wf-config-devel
+    \ wlroots-devel wlroots wf-config wf-config-devel umr radeontop rocm-smi
+    \ googler aha alot
     sudo dnf install -y pulseaudio-libs-devel libxcb-devel openssl-devel ncurses-devel dbus-devel
 
 
@@ -48,8 +49,11 @@ install_fedora() {
     \ gitui firefox emacs diskonaut cockpit-podman apvlv attr aria2 asciinema ack
     \ alacritty-terminfo cage gtk-layer-shell wayland-utils tiny lxc
     \ firefox-wayland spotify-tui spotifyd git-delta grip syncthing wabt mujs leveldb vile
-    \ binaryen supercollider faust youtube-dl abook
+    \ binaryen supercollider faust youtube-dl abook qconf kf5-knewstuff-devel
+    \ latencytop grsync hotspot ddd ddcontrol-gtk qoauth protobuf flatbuffers fann
+
     sudo dnf install -y zsh-autosuggestions zsh-syntax-highlight
+    sudo dnf install gtkglext-libs   libdazzle typelib-1_0-Retro-1-0  uget spread-sheet-widget libreport-gtk libpeas-gtk libinfinity-gtk libinfinity-gtk-devel  libchamplain-gtk
     # install exodus
 
     echo "Installing dev tools/languages..."
@@ -58,12 +62,26 @@ install_fedora() {
     \ haskell-platform gdb ghostscript gcc gcc-c++ docker-compose ctags ctags-etags
     \ cmake clojure cloc clang clang-devel ccls catimg buildah aws aws-doc ansible
     \ ansible-bender anaconda R R-core golang racket texlive-scheme-medium pandoc mathjax
-    \ fswatch entr
+    \ fswatch entr cockpit-selinux qt5-qtquickcontrols-examples fluid qqc2-desktop-style
+    \ libgnomeui libgnomecanvasmm26 qt5-designer kf5-kross-ui kf5-sonnet-ui
+    \ kf5-kguiaddons-devel qt5-qttools-libs-designer qt-designer-plugin-webkit qt5-qttools-libs-designercomponents qtkeychain
+    sudo dnf install R-sodium
 
 
     echo "Installing miscellaneous..."
     sudo dnf install -y fontawesome-fonts fira-code-fonts bspwm bpytop awesome
     \ awesome-vim-colorschemes avahi-tools nmap
+    \ qt5-qtdeclarative qt5-qtdeclarative-devel qt5-qtdeclarative-examples qwt
+    \ qwtplot3d qwt-qt5 qwt-qt5-devel qt5-ct qt5-qtcanvas3d qt5-qtconfiguration
+    \ neovim-qt glade-devel kf5-knewstuff gtk+extra gtk+extra-devel libhandy libsexy
+    \ gtk-layer-shell granite avahi-ui eclipse-swt gsequencer-devel gtk-layer-shell
+    \ libinfinity cegui xbae libfm-gtk libxfce4ui anaconda-widgets
+    \ libgnomeui libyui daw hyena gnet2 nunit gnet2-devel
+    \ kwin-common wayland-logout waylandpp waylandpp-devel weston wlcs
+    \ egl-wayland egl-wayland-devel ibus-wayland qt5-qtwayland-devel qt5-qtwayland-examples
+    # look up wayland libs
+    # maybe: ming64-goocanvas2 ? has a lot of different deps.
+    # maybe gcr gd ghc-vty gnet2 gnet2-devel
 
 }
 
@@ -94,6 +112,12 @@ install_from_cargo() {
     \ cargo-make cargo-flamegraph cargo-watch cargo-edit cargo-wasi
     \ carco-cache ffsend bandwhich spotify-tui spotifyd ncspot
     \ simple-http-server
+}
+
+install_from_flatpak() {
+    flatpak install runtime/org.freedesktop.Sdk.Extension.rust-nightly/x86_64/20.08
+    flatpak install zig
+    flatpak install GNOME
 }
 
 install_from_copr() {
